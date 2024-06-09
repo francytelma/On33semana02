168b8c216c7f6427eed1903b469b704b6717300a
Francitema <francytelma@hotmail.com>
2024-06-09 12:39:00 -0300

    Aula s02

---
+++ app.py
@@ -0,0 +1,1 @@
+print('hello On33, vamos reprogramar o mundo!')
---
+++ media.py
@@ -0,0 +1,12 @@
+def media (nota1, nota2):
+    resultadoMedia = (nota1 + nota2)/2
+    print('nota 1:',nota1)
+    print('nota 2:', nota2)
+    print('a média das notas é:', resultadoMedia)
+
+    #notas iniciais 
+nota1 = 8
+nota2 = 9
+
+    #chama a função média com as notas
+media(nota1,nota2)
